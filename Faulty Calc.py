165cc034a04900146645514bdc51633d76b1916c
trivedimargiv9 <70792623+trivedimargiv9@users.noreply.github.com>
2020-09-05 11:39:09 +0530

    Add files via upload

---
+++ Faulty Calc.py
@@ -0,0 +1,40 @@
+operater = input('Enter the operator: ')
+num1 = int(input('Enter number 1: '))
+num2 = int(input('Enter number 2: '))
+
+if operater == '+':
+
+    if num1 == 56 and num2 == 9:
+        print('77')
+    else:
+        print(num1+num2)
+
+elif operater == '-':
+    print(num1 - num2) 
+
+elif operater == '*':
+    if num1 == 45 and num2 == 3:
+        print('555')
+    else:
+        print(num1*num2)
+
+else:
+    if num1 == 56 and num2 == 6:
+        print('4')
+    else:
+        print(num1/num2)
+
+
+
+
+
+
+
+# if num1 == 45 and num2 == 3 and operater == '*':
+#     return 555
+
+# elif num1 == 56 and num2 == 9 and operater == '+':
+#     return 77
+
+# elif num1 == 56 and num2 == 6 and operater == '/':
+#     return 4
